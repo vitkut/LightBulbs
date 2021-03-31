4fd9f2162b04e483dfcf974344fe9961691e25fe
vitkut <68354033+vitkut@users.noreply.github.com>
2021-03-31 15:08:42 +0300

    First commit

---
+++ src/main/java/com/lightBulbs/lightBulbs/LightBulbsApplication.java
@@ -0,0 +1,13 @@
+package com.lightBulbs.lightBulbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightBulbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightBulbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lightBulbs/lightBulbs/LightBulbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightBulbs.lightBulbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightBulbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
